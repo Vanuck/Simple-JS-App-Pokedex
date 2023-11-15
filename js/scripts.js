3cb29fb24bdbbe9c69701d4af1614bd2e86ee260
Vanuck <56781279+Vanuck@users.noreply.github.com>
2023-11-15 10:10:33 -0800

    Update scripts.js
    updated to include item numbers and reformatted code to make easier reading.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
-// Variables // 
+// Variables
+// Height is in meters
+
 let pokemonList = [
-    {name: 'Wartortle', height: 1, types: 'Water'},
-    {name: 'Primeape', height: 1, types: 'Fighting'},
-    {name: 'Ekans', height: 2, types: 'Poison'}
+    {
+        item: 1,
+        name: 'Wartortle', 
+        height: 1, 
+        type: ['water']
+    },
+    {
+        item: 2,
+        name: 'Primeape', 
+        height: 1, 
+        type: ['fighting']
+    },
+    {
+        item:3
+        name: 'Ekans', 
+        height: 2, 
+        type: ['poison']
+    },
 ];
