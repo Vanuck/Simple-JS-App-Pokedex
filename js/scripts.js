8527f931cdad2dc916ec1414e4fdc20906b9e0e6
Vanuck <56781279+Vanuck@users.noreply.github.com>
2023-11-15 10:01:53 -0800

    Create Pokemonlist
    Create Variable pokemonlist and objects in array.
    added 3 Keys to 3 objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
 // Variables // 
-let favoriteFood = 'Donair';
-document.write(favoriteFood) ; 
+let pokemonList = [
+    {name: 'Wartortle', height: 1, types: 'Water'},
+    {name: 'Primeape', height: 1, types: 'Fighting'},
+    {name: 'Ekans', height: 2, types: 'Poison'}
+];
