f4775fdb9bbb5e24fd05e7144b4afb6f598e7aa3
Vanuck <56781279+Vanuck@users.noreply.github.com>
2023-11-16 15:07:39 -0800

    Updated Scripts.js
    Put in definition of condition for taller pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,8 @@ let pokemonList = [
 
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height > 1.5)
-     {document.write(pokemonList[i].name + "  " + "(height:"+ "  " +  pokemonList[i].height + " ) " + "- Wow. That\'s big!"+ "<br>"); //condition for pokemon with the height > 1 
+     {document.write(pokemonList[i].name + "  " + "(height:"+ "  " +  pokemonList[i].height + " ) " + "- Wow. That's big!"+ "<br>"); 
+     //condition put in for pokemon with the height taller than 1.5 
   } else {
   document.write(pokemonList[i].name + "  " + "(height:"+ "  " +  pokemonList[i].height + " ) " + "<br>");}
   }
