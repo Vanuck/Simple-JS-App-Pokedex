c6fb80b844b957b965879fb6cc2577d6bb92d32d
Vanuck <56781279+Vanuck@users.noreply.github.com>
2023-11-16 16:25:48 -0800

    Updated scripts.js
    Modified For loop with height sizing for exercise.

--- js/scripts.js
+++ js/scripts.js
@@ -4,25 +4,24 @@
 let pokemonList = [
     {
         name: 'Wartortle', 
-        height: 1, 
+        height: 1.0,
         type: ['water']
     },
     {
-        name: 'Primeape', 
-        height: 1, 
-        type: ['fighting']
+        name: 'Ekans', 
+        height: 2.0, 
+        type: ['poison']
     },
     {
-        name: 'Ekans', 
-        height: 2, 
-        type: ['poison']
+        name: 'Primeape', 
+        height: 1.0, 
+        type: ['fighting']
     },
 ];
 
 for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 1.5)
-     {document.write(pokemonList[i].name + "  " + "(height:"+ "  " +  pokemonList[i].height + " ) " + "- Wow. That's big!"+ "<br>"); 
-     //condition put in for pokemon with the height taller than 1.5 
+    if (pokemonList[i].height >1.0)
+     {document.write(pokemonList[i].name + "  " + "(height:" + "  " +  pokemonList[i].height + " ) " + " Wow. That's big!"+ "<br>"); //condition put in for pokemon with the height taller than 1.0 
   } else {
   document.write(pokemonList[i].name + "  " + "(height:"+ "  " +  pokemonList[i].height + " ) " + "<br>");}
   }
