39ed11324c0c8159fb6fa51910f508499c0e5cbf
Vanuck <56781279+Vanuck@users.noreply.github.com>
2023-11-22 12:33:02 -0800

    Added IIFE to scripts.js
    Added IIFE to scripts.js
    started working on search function parameters

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+//new pokemonRepository variable to hold what IIFE will return
+let pokemonRepository = (function () {
+
 // Variables
 // Height is in meters
 
@@ -19,12 +22,48 @@ let pokemonList = [
     },
 ];
 
-pokemonList.forEach(function(user) {
-    if (pokemonList.height >1.0)
-    {document.write(user.name + " " + "(height:" + user.height + ") " + " Wow. That's big!" + "<br>"); //condition put in for pokemon with the height taller than 1.0 
-  } else {
-    {document.write(user.name + " " + "(height:" + user.height + ") " +"<br>");}
-  }
+//IIFE will return objects with these publis functions
+function getALL() {
+  return pokemonList;
+}
+
+//   adding pokemon based on the requirements
+function add(pokemon){
+  //  checking whether each pokemon is an object and checks keys of the pokemon object are present 
+  if(typeof pokemon === 'object' && 
+  "name" in pokemon){
+  // "name" in pokemon
+  //"height" in pokemon
+  // "type" in pokemon 
+  
+      pokemonList.push(pokemon);
+  }else{
+      console.log("Pokemon is not correct")};
+  }    
+  //function add (pokemon) {
+  //pokemonList.push(pokemon);
+//}
+
+return {
+  add: add,
+  getALL: getALL
+};
+
+})();
+
+// forEach() loop that iterates over each Pokémon in the repository
+pokemonRepository.getALL().forEach(function (pokemon) {
+    document.write('<p>' + pokemon.name + " " + "(height: " + pokemon.height + ")" + '</p>')
+})
+
+
+
+//pokemonList.forEach(function(user) {
+   // if (pokemonList.height >1.0)
+    //{document.write(user.name + " " + "(height:" + user.height + ") " + " Wow. That's big!" + "<br>"); //condition put in for pokemon with the height taller than 1.0 
+ // } else {
+ //   {document.write(user.name + " " + "(height:" + user.height + ") " +"<br>");}
+//  }
 
 
 //for (let i = 0; i < pokemonList.length; i++) {
@@ -32,5 +71,4 @@ pokemonList.forEach(function(user) {
    //  {document.write(pokemonList[i].name + "  " + "(height:" + "  " +  pokemonList[i].height + " ) " + " Wow. That's big!"+ "<br>"); //condition put in for pokemon with the height taller than 1.0 
  // } else {
  // document.write(pokemonList[i].name + "  " + "(height:"+ "  " +  pokemonList[i].height + " ) " + "<br>");}
- // }
-})
+ //}
